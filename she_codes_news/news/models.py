626fe13b4a897e8b6e8e5380f1fddbe2e09f81c2
Mara Petra <62734302+mariapetra@users.noreply.github.com>
2022-02-08 20:28:50 +0800

    added stories to user profile and ability to create user account

--- she_codes_news/news/models.py
+++ she_codes_news/news/models.py
@@ -4,7 +4,6 @@ from django.db import models
 
 class NewsStory(models.Model):
     title = models.CharField(max_length=200)
-    # author = models.CharField(max_length=200)
     author = models.ForeignKey(
         get_user_model(),
         on_delete=models.CASCADE
--- she_codes_news/users/views.py
+++ she_codes_news/users/views.py
@@ -1,8 +1,12 @@
-from multiprocessing import context
+# from multiprocessing import context
 from re import template
 from django.shortcuts import render
+from django.urls import reverse_lazy
+
+from .forms import CustomUserCreationForm
 from django.views import generic
 from django.contrib.auth import get_user_model
+from news.models import NewsStory
 
 USER_MODEL = get_user_model()
 # Create your views here.
@@ -10,3 +14,13 @@ class ProfileView(generic.DetailView):
     model = USER_MODEL
     template_name = 'users/profile.html'
     context_object_name = 'user'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['user_story'] = NewsStory.objects.filter(author=self.kwargs['pk'])
+        return context
+
+class CreateAccountView(generic.CreateView):
+     form_class = CustomUserCreationForm
+     success_url = reverse_lazy('login')
+     template_name = "users/createAccount.html"
