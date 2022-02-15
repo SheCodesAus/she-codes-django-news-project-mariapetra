3192f19e9760a42c73d8d71e29a4eda3e980c1d6
Mara Petra <62734302+mariapetra@users.noreply.github.com>
2022-02-15 14:52:46 +0800

    added ability to add image url

--- she_codes_news/news/models.py
+++ she_codes_news/news/models.py
@@ -10,10 +10,10 @@ class NewsStory(models.Model):
     )
     pub_date = models.DateTimeField()
     content = models.TextField() 
+    image=models.URLField(null=True, blank=True)
+
     class Meta: 
         ordering = ['-pub_date']
-
-
 
     categories = (
         ("NEWS", "News"),
@@ -22,3 +22,4 @@ class NewsStory(models.Model):
     )
 
     category = models.CharField(max_length=200, choices = categories, default="News")
+
